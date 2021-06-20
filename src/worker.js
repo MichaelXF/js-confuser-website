81d76f08330db84b43424c2c48b91fbb3bce06e0
MichaelXF <minimike3682@gmail.com>
2021-06-20 18:18:05 -0400

    Progress bar support

--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,13 @@
 const JsConfuser = require("js-confuser");
 
 export const obfuscate = (code, options) => {
-  JsConfuser.obfuscate(code, options)
+  var callback = (name, complete, total) => {
+    postMessage({ event: "progress", data: [name, complete, total] });
+  };
+
+  JsConfuser.debugObfuscation(code, { ...options, verbose: true }, callback)
     .then((obfuscated) => {
+      console.log("Successfully obfuscated code");
       postMessage({ event: "success", data: obfuscated.toString() });
     })
     .catch((error) => {
